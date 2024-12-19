85f3427219af8225749680d0836f123f4dee5698
salma <salma@DESKTOP-BIS03H7>
2024-12-19 22:49:17 +0100

    show raw results

--- QuizDickTionary/Application/Models/WordWithAnswer.cs
+++ QuizDickTionary/Application/Models/WordWithAnswer.cs
@@ -10,9 +10,16 @@ namespace QuizDickTionary.Application.Models
     public class WordWithAnswer
     {
         public bool IsCorrect { get; }
+        public string EnglishWord { get; set; }
+        public string HungarianhWord { get; set; }
+        public string Answer { get; set; }
+
         public WordWithAnswer(WordDto wordDto, string answer)
         {
-            //Make this generic for future depending on settings languange
+            //Make this generic for future depending on settings
+            EnglishWord = wordDto.English;
+            HungarianhWord = wordDto.Hungarian;
+            Answer = answer;
             if (wordDto.English == answer)
             {
                 IsCorrect = true;
