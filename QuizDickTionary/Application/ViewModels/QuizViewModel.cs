f44cd33ec04bb8e85bad52b9db332d35ad8c7bd9
salma <salma@DESKTOP-BIS03H7>
2024-12-19 21:22:32 +0100

    Add button on start quiz and iterate throu questions

--- QuizDickTionary/Application/ViewModels/QuizViewModel.cs
+++ QuizDickTionary/Application/ViewModels/QuizViewModel.cs
@@ -9,6 +9,17 @@ namespace QuizDickTionary.Application.ViewModels
     {
         private IViewModelFactory _viewModelFactory;
         public int CountOfQuestions { get; set; } = 5;
+        private int _crurrentQuiazQuestion;
+        private List<WordViewModel> _submitedAnswers = new List<WordViewModel>();
+        public int CurrentQuizQuestion
+        {
+            get { return _crurrentQuiazQuestion; }
+            set { InternalSetPropertyValue(ref _crurrentQuiazQuestion, value); }
+        }
+        public Command SubmitCommand { get; set; }
+
+        private Timer _timer;
+
         public string QuestionIndex { get; set; } = "hellooo";
         public ObservableCollection<WordViewModel> ObservableWordsViewModel { get; }
         private WordViewModel _question;
@@ -19,11 +30,28 @@ namespace QuizDickTionary.Application.ViewModels
             set { InternalSetPropertyValue(ref _question, value); }
         }
 
-        public QuizViewModel(IViewModelFactory viewModelFactory) : base (viewModelFactory)
+        private string _submitButtonText = "Submit";
+
+        public string SubmitButtonText
+        {
+            get { return _submitButtonText; }
+            set { InternalSetPropertyValue(ref _submitButtonText, value); }
+        }
+
+        private TimeOnly _currentTime;
+
+        public TimeOnly CurrentTime
+        {
+            get { return _currentTime; }
+            set { _currentTime = value; }
+        }
+
+        public QuizViewModel(IViewModelFactory viewModelFactory) : base(viewModelFactory)
         {
             ObservableWordsViewModel = new ObservableCollection<WordViewModel>();
             _viewModelFactory = viewModelFactory;
             LoadQuizViewModels();
+            SubmitCommand = new Command(SubmitAnswer);
         }
 
         private async void LoadQuizViewModels()
@@ -35,8 +63,48 @@ namespace QuizDickTionary.Application.ViewModels
                 WordViewModel wordViewModel = new WordViewModel(_viewModelFactory);
                 wordViewModel.InitializeContent(word);
                 ObservableWordsViewModel.Add(wordViewModel);
-                Question = wordViewModel;
             }
+            Question = ObservableWordsViewModel[CurrentQuizQuestion];
+        }
+
+        private void SubmitAnswer()
+        {
+            _submitedAnswers.Add(ObservableWordsViewModel[_crurrentQuiazQuestion]);
+            SafeIncementation();
+            if (IsFinishPressed())
+            {
+                // new view with summary 
+                ElaborateResults();
+                return;
+            }
+
+            Question = ObservableWordsViewModel[_crurrentQuiazQuestion];
+        }
+
+        private void ElaborateResults()
+        {
+            foreach (var question in _submitedAnswers)
+            {
+
+            }
+        }
+
+        private void SafeIncementation()
+        {
+            if (CountOfQuestions - 1 > _crurrentQuiazQuestion)
+            {
+                CurrentQuizQuestion++;
+            }
+        }
+
+        private bool IsFinishPressed()
+        {
+            if (CountOfQuestions - 1 == _crurrentQuiazQuestion)
+            {
+                SubmitButtonText = "Finish";
+                return true;
+            }
+            return false;
         }
     }
 }
